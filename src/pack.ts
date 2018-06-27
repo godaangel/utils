0cdc1ad3edeaf94f44cb3b82b941ea86b9f99a13
warrenyang <warrenyang@tencent.com>
2018-06-27 19:34:37 +0800

    新增类型判断和随机数生成

--- src/pack.ts
+++ src/pack.ts
@@ -1,9 +1,14 @@
 import Find from './core/find/index'
 import IsSameObj from './core/compare/object'
 import TimeUtils from './core/time/index'
+import IsType from './core/type/index'
+import {RandomStr, RandomNum} from './core/random/index'
 
 export {
 	Find,
 	IsSameObj,
-	TimeUtils
+	TimeUtils,
+	IsType,
+	RandomStr,
+	RandomNum
 }
